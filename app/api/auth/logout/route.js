5a6f40936011586c167b478b1d0cbbfcc18a9b9d
shimanshucs18 <sharmatupwork@gmail.com>
2026-06-27 00:11:24 +0530

    Clean up logout route code

--- app/api/auth/logout/route.js
+++ app/api/auth/logout/route.js
@@ -1,20 +1,12 @@
-// import { NextResponse } from "next/server"
-
-// export async function POST() {
-//   const response = NextResponse.redirect(new URL("/login", "http://localhost:3000"))
-//   response.cookies.set("token", "", { maxAge: 0 })
-//   return response
-// }
-
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
 export async function POST(req) {
-  const url = new URL("/login", req.url)
-  const response = NextResponse.redirect(url)
-  
+  const url = new URL("/login", req.url);
+  const response = NextResponse.redirect(url);
+
   // Both cookies delete
-  response.cookies.set("token", "", { maxAge: 0 })
-  response.cookies.set("refresh_token", "", { maxAge: 0 })
-  
-  return response
+  response.cookies.set("token", "", { maxAge: 0 });
+  response.cookies.set("refresh_token", "", { maxAge: 0 });
+
+  return response;
 }
